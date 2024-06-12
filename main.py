f9da69b53309ab8adede4a2dd568714ba349b2c1
grhamm-pabst <grhammpabst456@gmail.com>
2024-06-12 18:42:18 -0300

    feat: connection with gemini

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+import google.generativeai as genai
+import os
+from dotenv import load_dotenv, dotenv_values
+
+load_dotenv()
 
 app =  FastAPI()
 
-@app.get("/")
-def hello_world():
-    return {"Hello": "Bindson"}
+@app.get("/{prompt}")
+def hello_world(prompt):
+    genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+    model = genai.GenerativeModel('gemini-1.0-pro-latest')
+    
+    response = model.generate_content(prompt)
+    
+    return { "Response": response.text }
