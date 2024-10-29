22cc1176842c1efa4327c401ea028569409b0a4c
grhamm-pabst <grhammpabst456@gmail.com>
2024-10-29 20:17:59 -0300

    feat: clustering with meanshift

--- ranking.py
+++ ranking.py
@@ -1,6 +1,6 @@
 from transformers import pipeline
-import numpy
-
+import numpy as np
+from sklearn.cluster import MeanShift, estimate_bandwidth
 class Ranking:
     def __init__(self):
         self.classifier = pipeline('zero-shot-classification', model='roberta-large-mnli')
@@ -12,13 +12,18 @@ class Ranking:
         result = self.classifier(sequence_to_classify, terms, hypothesis_template=hypothesis_template)
 
         return self.format_response(result)
+    
+    def group_scores(self, scores, terms):
+        X = np.array(scores).reshape(-1, 1)
+        bandwidth = estimate_bandwidth(X, quantile=0.3)
+        ms = MeanShift(bandwidth=bandwidth, bin_seeding=True)
+        ms.fit(X)
+        labels = ms.labels_
 
-    def response_tuple_to_dict(self, term, level):
-        
-        return {
-            "term": term,
-            "level": level
-        }
+        labels_unique = np.unique(labels)
+        n_clusters_ = len(labels_unique)
+
+        return [{ "term": terms[i], "level": n_clusters_ - labels[i].item() } for i in range(len(labels))]
 
     def format_response(self, response):
-        return list(map(self.response_tuple_to_dict, response["labels"], response["scores"]))
+        return self.group_scores(response["scores"], response["labels"])
